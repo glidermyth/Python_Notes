0b301cba9c7685ff82f96cf0fa6b68d10fda2896
Glider Myth <glidermyth@gmail.com>
2022-11-27 00:03:04 +0530

    Type Conversion in python

---
+++ 06_Type_Conversion.py
@@ -0,0 +1,12 @@
+# The following shows the most important ones for now:
+
+# int(str) - convert a string to a integer number.
+# float(str) – convert a string to a floating-point number.
+# bool(val) – convert a value to a boolean value, either True or False.
+# str(val) – return the string representation of a value.
+
+price = input("Enter the price : ")
+print(type(price))
+
+price = int(price)
+print(type(price))
