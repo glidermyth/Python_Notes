96f2bf3e3d4175c9082d8f482de302aac0bfc336
Glider Myth <glidermyth@gmail.com>
2022-11-27 11:02:35 +0530

    Logical Operator in Python

---
+++ 08_Logical_Operator.py
@@ -0,0 +1,24 @@
+# Python has three logical operators:
+
+# and
+# or
+# not
+
+a_val = True
+b_val = True
+
+print(a_val and b_val)
+# The and operator returns True if both conditions are True. 
+# And it returns False if either the condition a or b is False.
+
+
+b_val = False
+
+print(a_val or b_val)
+# the or operator checks multiple conditions. But it returns True when either or both individual conditions are True
+# The or operator returns False only when both conditions are False.
+
+
+print(not a_val)
+# The not operator applies to one condition. 
+# And it reverses the result of that condition, True becomes False and False becomes True.
