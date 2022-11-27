834f5130c6a683f975de7faabd6af206ea77a26f
Glider Myth <glidermyth@gmail.com>
2022-11-27 10:56:40 +0530

    Comparison operator in python

---
+++ 07_Comparison_Operator.py
@@ -0,0 +1,16 @@
+# Python has six comparison operators, which are as follows:
+
+# Less than ( < )
+# Less than or equal to (<=)
+# Greater than (>)
+# Greater than or equal to (>=)
+# Equal to ( == )
+# Not equal to ( != )
+# These comparison operators compare two values and return a boolean value, either True or False.
+
+# And you can use these comparison operators to compare both numbers and strings.
+
+print( 10 < 20)
+
+print('apple' < 'orange')
+# The expression 'apple' < 'orange' returns True because the letter a in apple is before the letter o in orange.
